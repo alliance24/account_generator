73c58dfc76fdcaa04503ccfd9cf9c5acba6ee6f4
alliance24 <70445622+alliance24@users.noreply.github.com>
2022-11-28 00:46:13 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
-from random import *
+from openpyxl import Workbook, load_workbook
+from win10toast import ToastNotifier
+import random
 import os
-import csv
 
-sample_csv = "Prenoms.csv"
+wb = Workbook()
+ws = wb.active
+ws.title = "Accounts"
+#print(wb.sheetnames)
 
-# nb = int(input("Combien de comptes ? "))
-# os.remove("account.txt")
-# file = open("account.txt", "a+")
-# file.close
+n = ToastNotifier()
+nb = int(input("Combien de comptes ? "))
+#file = open("accounts.txt", "a+")
 
-with open(sample_csv, "r") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for line in csv_reader:
-        print(line)
+names_file = open("noms.txt", "r")
+lines = names_file.readlines()
+def nom():
+    liste = []
+    for line in lines:
+        liste.append(line.strip())
+    names_file.close
+    return random.choice(liste)
+
+firsts_name_file = open("prenoms.txt", "r")
+lines = firsts_name_file.readlines()
+def prenom():
+    liste = []
+    for line in lines:
+        liste.append(line.strip())
+    firsts_name_file.close
+    return random.choice(liste)
+
+def birth_date():
+    day = random.randint(1, 30)
+    month = random.randint(1, 12)
+    year = random.randint(1950, 2010)
+    date = f"{day}/{month}/{year}"
+    return date
+
+#file.write("Mail        Nom     Prenom      Date de naissance\n")
+ws.append(["Mail", "Nom", "Prenom", "Date de naissance"])
+print()
+min = 0
+for i in range(nb):
+    prenom1 = prenom()
+    nom1 = nom()
+    date = birth_date()
+    mail = prenom1 + "." + nom1 +"@gmail.com"
+    #file.write(prenom1 + "." + nom1 +"@gmail.com   " + nom1 + "    " + prenom1 + "  " + date + "\n")
+    ws.append([mail, nom1, prenom1, date])
+    pourcent = int((i/nb)*100)
+    if pourcent > min:
+        print(pourcent, "%")
+        min = pourcent
+#file.close
+wb.save("accounts.xlsx")
+print("100 %")
+print(f"Génération de {nb} comptes terminée...")
+n.show_toast("Account_generator", "La génération des comptes est terminée", duration = 10)
+
+
